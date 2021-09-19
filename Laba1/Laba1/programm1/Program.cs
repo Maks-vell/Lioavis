338addf9c63076ad6d68133d54e42e4ee2e86233
Maks <maks_25@mail.ru>
2021-09-19 21:55:12 +0300

    init commit

---
+++ Laba1/Laba1/programm1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace programm1
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Arr1();
+
+            Arr2();
+            
+        }
+
+        private static void Arr1()
+        {
+            Console.WriteLine("Enter the size of array: ");
+            int size = int.Parse(Console.ReadLine() ?? string.Empty);
+            int[] arr = new int[size];
+            Random rnd = new Random();
+
+            for (int i = 0; i < size; i++) //заполнение 
+            {
+                arr[i] = rnd.Next(10);
+            }
+
+            for (int i = 0; i < size; i++) //вывод
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            int max = arr[0]; //подсчет max min
+            int min = arr[0];
+            foreach (int element in arr)
+            {
+                if (element > max) max = element;
+                if (element < min) min = element;
+            }
+
+            Console.WriteLine(max - min);
+        }
+
+        private static void Arr2()
+        {
+            Console.WriteLine("Enter the number of string in array: "); //ввод
+            int x = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine("Enter the number of columm in array: ");
+            int y = int.Parse(Console.ReadLine() ?? string.Empty);
+            int[,] arr = new int[x, y];
+            Random rnd = new Random();
+
+            for (int i = 0; i < x; i++) //заполнение
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    arr[i, j] = rnd.Next(10);
+                }
+            }
+
+            for (int i = 0; i < x; i++) // вывод
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+
+                Console.WriteLine(" ");
+            }
+
+            int sum = 0;
+            Console.WriteLine("sums: "); //подсчет и вывод сумм
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    sum += arr[i, j];
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            
+        }
+    }
+}
---
+++ Laba1/Laba1/programm2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public struct Student
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Birthday { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+    }
+    
+    class Program
+    {
+        private static Student[] _students = new Student[5];
+
+        static void Main(string[] args)
+        {  
+            _students[0].Name ="Maksim";
+            _students[0].Family = "Vyalmisov";
+            _students[0].Birthday = 2003;
+            _students[0].Course = 2;
+            _students[0].Group = "20VV1";
+            
+            _students[0].Name ="Pavel";
+            _students[0].Family = "Ivkin";
+            _students[0].Birthday = 2002;
+            _students[0].Course = 2;
+            _students[0].Group = "20VO1";
+
+            _students[0].Name ="lol";
+            _students[0].Family = "kek";
+            _students[0].Birthday = 1;
+            _students[0].Course = 0;
+            _students[0].Group = "____";
+            
+            Console.Write("Enter name: ");
+            string enteredName = Console.ReadLine();
+            Console.Write("Enter family: ");
+            string enteredFamily = Console.ReadLine();
+            Console.Write("Enter birthday: ");
+            int enteredBirthday = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            bool founded=false;
+            foreach (Student student in _students)
+            {
+                if (student.Name == enteredName && student.Family == enteredFamily &&
+                    student.Birthday == enteredBirthday)
+                {
+                    Console.WriteLine("Student found: ");
+                    Console.WriteLine("Course:" + student.Course);
+                    Console.WriteLine("Group: "+ student.Group);
+                    founded = true;
+                }
+            }
+
+            if (!founded)
+            {
+                Console.WriteLine("Student not found");
+            }
+            
+        }
+    }
+    
+}
