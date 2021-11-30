481b8015400bb828f999b20940ba78e15f57d8a3
Maks <maks_25@mail.ru>
2021-11-30 20:40:29 +0300

    laba4

---
+++ Laba4/Laba4_/Laba3_/Graphs/ListGraph/ListGraphStatic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba4_.Graphs
+{
+    partial class ListGraph
+    {
+        public static void Display(ListGraph graph)
+        {
+            List<List<int>> list = graph.List;
+            foreach (List<int> v in list)
+            {
+                if (v.Count == 0) Console.Write("-");
+                for (int j = 0; j < v.Count; j++)
+                {
+                    Console.Write(v[j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Laba4/Laba4_/Laba3_/LMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Laba4_
+{
+    class LMain
+    {
+        static void Main(string[] args)
+        {
+            ConsoleProgram consoleProgram = new ConsoleProgram();
+            int operation;
+
+            consoleProgram.WriteInfo();
+
+            do
+            {
+                Console.WriteLine();
+                Console.Write("\t \t \t Введите номер операции: ");
+                operation = int.Parse(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        consoleProgram.CreateGrahs();
+                        break;
+                    case 2:
+                        consoleProgram.VertexContraction();
+                        break;
+                    case 3:
+                        consoleProgram.SplitVertex();
+                        break;
+                    case 4:
+                        consoleProgram.UnionGraphs();
+                        break;
+                    case 5:
+                        consoleProgram.CrossingGraphs();
+                        break;
+                    case 6:
+                        consoleProgram.AnnularSum();
+                        break;
+                    case 7:
+                        consoleProgram.DecartSum();
+                        break;
+                    case 8:
+                        consoleProgram.DeepWalk();
+                        break;
+                    case 9:
+                        consoleProgram.DeepWalkNonRecurcive();
+                        break;
+                    default:
+                        Console.WriteLine("Неизвестная операция ");
+                        break;
+                }
+            } while (operation != 0);
+
+            Console.WriteLine();
+            Console.WriteLine("\t \t \t Работа завершена.");
+        }
+    }
+}
